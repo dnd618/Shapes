954a1da862a20db18f45c72bd6fc50115d143c04
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-07-28 15:42:54 -0500

    weekend push

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 class Shapes {
     constructor(width, height, radius, side) {
         this.width = width;
@@ -34,3 +36,43 @@ class Triangle extends Shapes {
         this.height = true;
     }
 } 
+
+let Shapes = function() { };
+   
+  Shape.prototype = {
+    // x: 30,
+    // y: 30,
+    width: 25,
+    height: 25,
+     
+    draw: function(ctx) {
+      throw new Error("This is parent method");
+    },
+     
+    print: function() {
+      console.log(this.width + '<>' + this.height);
+    }
+  };
+  //this.x + ':' + this.y + ' ' + --cut from print function
+   
+  var Circle = function() { this.radius = 20; };
+  Circle.prototype = new Shape();
+  Circle.prototype.draw = function(ctx) {
+    ctx.arc(12, 22, 66, 0, 2 * Math.PI, false);
+  };
+
+
+//In class example
+class Shapes {
+    constructor(width, height, radius, side) {
+        this.shape = document.createElement('div');
+        this.shape.className = 'Triangle';
+            document.body.appendChild(Circle);
+//draw shape
+class Triangle extends Shapes {
+    constructor(height) {
+        super('Triangle');
+        this.draw
+    }
+}
+};
